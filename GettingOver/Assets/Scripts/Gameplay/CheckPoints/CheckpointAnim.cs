902ab72629e80bce59719d73d564476d5eb4ad83
KogNo1 <boy231193@gmail.com>
2018-02-05 09:42:34 +0700

    flag Animation by Tung

---
+++ GettingOver/Assets/Scripts/Gameplay/CheckPoints/CheckpointAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointAnim : MonoBehaviour {
+
+	[SerializeField]
+	float timeAnim;
+
+	int currentAnim;
+
+	[SerializeField]
+	List<Sprite> flagAnim = new List<Sprite>();
+
+	[SerializeField]
+	SpriteRenderer flag;
+
+	// Use this for initialization
+	void Start () {
+		currentAnim = 0;
+		StartCoroutine (FlagDoAnim (timeAnim));
+	}
+	
+	IEnumerator FlagDoAnim(float time){
+		yield return new WaitForSeconds (time);
+		if (currentAnim < flagAnim.Count - 1) {
+			currentAnim++;
+		} else {
+			currentAnim = 0;
+		}
+		flag.sprite = flagAnim [currentAnim];
+		StartCoroutine (FlagDoAnim (timeAnim));
+	}
+}
