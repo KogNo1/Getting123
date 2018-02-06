aafa90087fd43b5fd48fc442e7cc5361d9514861
ManhNguyen <manh.nguyen.kod@gmail.com>
2018-02-06 14:51:53 +0700

    Sound Manager by Manh

---
+++ GettingOver/Assets/Scripts/Others/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static AudioSource Hits;
+	public static AudioSource Clicks;
+	public static AudioSource Coins;
+
+	public AudioSource Hit;
+	public AudioSource Click;
+	public AudioSource Coin;
+
+	// Use this for initialization
+	void Start () {
+		Hits = Hit;
+		Clicks = Click;
+		Coins = Coin;
+	}
+
+	public static void MuteAll(){
+		Hits.mute = true;
+		Clicks.mute = true;
+		Coins.mute = true;
+	}
+
+	public static void DontMuteAll(){
+		Hits.mute = false;
+		Clicks.mute = false;
+		Coins.mute = false;
+	}
+}
