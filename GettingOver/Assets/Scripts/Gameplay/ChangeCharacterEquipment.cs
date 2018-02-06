eea5e8aaf70698324e5e434777f1992731c10cf3
ManhNguyen <manh.nguyen.kod@gmail.com>
2018-02-06 09:09:14 +0700

    Update GOB by manh

---
+++ GettingOver/Assets/Scripts/Gameplay/ChangeCharacterEquipment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacterEquipment : MonoBehaviour {
+
+	[Header("Sprite Hat")]
+	[SerializeField]
+	SpriteRenderer Hat;
+	[SerializeField]
+	Sprite[] hatSprite;
+
+	[Header("Sprite Foot")]
+	[SerializeField]
+	SpriteRenderer Foot;
+	[SerializeField]
+	Sprite[] footSprite;
+
+	// Use this for initialization
+	void Awake () {
+		Hat.sprite = hatSprite [SaveManager.instance.state.indexHat];
+		Foot.sprite = footSprite [SaveManager.instance.state.indexFoot];
+	}
+}
