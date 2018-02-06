49e150fc302638ad2b27ae68549abcc4a4d5f5ca
ManhNguyen <manh.nguyen.kod@gmail.com>
2018-02-06 09:51:06 +0700

    Update CHange weapon by Manh

--- GettingOver/Assets/Scripts/Gameplay/ChangeCharacterEquipment.cs
+++ GettingOver/Assets/Scripts/Gameplay/ChangeCharacterEquipment.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ChangeCharacterEquipment : MonoBehaviour {
 
@@ -16,8 +17,51 @@ public class ChangeCharacterEquipment : MonoBehaviour {
 	[SerializeField]
 	Sprite[] footSprite;
 
+	[Header("Weapon")]
+	[SerializeField]
+	GameObject[] weapons;
+
+	[Header("Character hinge joint")]
+	[SerializeField]
+	HingeJoint2D hingeOfCharacter;
+
+	[Header("EventTrigger Button Control")]
+	[SerializeField]
+	EventTrigger rightButton;
+	[SerializeField]
+	EventTrigger leftButton;
+
 	// Use this for initialization
 	void Awake () {
+		for (int i = 0; i < weapons.Length; i++) {
+			if (i == SaveManager.instance.state.indexWp) {
+				weapons [i].SetActive (true);
+				hingeOfCharacter.connectedBody = weapons [i].GetComponent<Rigidbody2D> ();
+
+				MoveHand moveHand = weapons [i].GetComponent<MoveHand> ();
+
+				EventTrigger.Entry entryMoveRight = new EventTrigger.Entry();
+				entryMoveRight.eventID = EventTriggerType.PointerDown;
+				entryMoveRight.callback.AddListener( (eventData) => { moveHand.MoveRight(); } );
+
+				rightButton.triggers.Add (entryMoveRight);
+
+				EventTrigger.Entry entryMoveLeft = new EventTrigger.Entry();
+				entryMoveLeft.eventID = EventTriggerType.PointerDown;
+				entryMoveLeft.callback.AddListener( (eventData) => { moveHand.MoveLeft(); } );
+
+				leftButton.triggers.Add (entryMoveLeft);
+
+				EventTrigger.Entry entryPointUp = new EventTrigger.Entry();
+				entryPointUp.eventID = EventTriggerType.PointerUp;
+				entryPointUp.callback.AddListener( (eventData) => { moveHand.pointUp(); } );
+
+				rightButton.triggers.Add (entryPointUp);
+				leftButton.triggers.Add (entryPointUp);
+			}
+			else
+				weapons [i].SetActive (false);
+		}
 		Hat.sprite = hatSprite [SaveManager.instance.state.indexHat];
 		Foot.sprite = footSprite [SaveManager.instance.state.indexFoot];
 	}
